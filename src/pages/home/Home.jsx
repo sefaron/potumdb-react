dcc6fd45b6600ce3907a7a0e11da39895c7e409c
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-06 01:28:46 +0600

    update home page styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,19 +4,19 @@ function Home() {
   return (
     <main className="flex flex-col gap-6 justify-center items-center my-8">
       <Link
-        className="block text-2xl px-4 py-2 w-full max-w-[320px] hover:scale-105 transition-transform transform-gpu duration-500 rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
+        className="block text-2xl p-[0.8rem] w-full max-w-[320px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
         to="food"
       >
         Food Buff
       </Link>
       <Link
-        className="block text-2xl px-4 py-2 w-full max-w-[320px] hover:scale-105 transition-transform transform-gpu duration-500 rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
+        className="block text-2xl p-[0.8rem] w-full max-w-[320px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
         to="food"
       >
         Quest Calulator
       </Link>
       <Link
-        className="block text-2xl px-4 py-2 w-full max-w-[320px] hover:scale-105 transition-transform transform-gpu duration-500 rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
+        className="block text-2xl p-[0.8rem] w-full max-w-[320px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
         to="food"
       >
         Bag Expansion
