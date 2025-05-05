13420701ccdc319edfe1e70d826575d5d319b9b5
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-05 23:11:45 +0600

    update bg in index

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Background from "./component/background/Background";
 import Header from "./component/header/Header";
 import { Routes, Route } from "react-router";
 import Home from "./pages/home/Home";
@@ -7,7 +6,6 @@ import Food from "./pages/food/Food";
 function App() {
   return (
     <>
-      <Background />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/component/background/Background.jsx
+++
@@ -1,7 +0,0 @@
-function Background() {
-  return (
-    <div className="absolute inset-0 bg-white dark:bg-neutral-950 z-[-1]"></div>
-  )
-}
-
-export default Background
