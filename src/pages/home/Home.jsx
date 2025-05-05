e1371357fb50e8c80fcadfc9574cb39128616671
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-05 23:48:56 +0600

    add transition to home links

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,19 +4,19 @@ function Home() {
   return (
     <main className="flex flex-col gap-6 justify-center items-center my-8">
       <Link
-        className="block text-2xl px-4 py-2 w-full max-w-[320px] rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
+        className="block text-2xl px-4 py-2 w-full max-w-[320px] hover:scale-105 transition-transform transform-gpu duration-500 rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
         to="food"
       >
         Food Buff
       </Link>
       <Link
-        className="block text-2xl px-4 py-2 w-full max-w-[320px] rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
+        className="block text-2xl px-4 py-2 w-full max-w-[320px] hover:scale-105 transition-transform transform-gpu duration-500 rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
         to="food"
       >
         Quest Calulator
       </Link>
       <Link
-        className="block text-2xl px-4 py-2 w-full max-w-[320px] rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
+        className="block text-2xl px-4 py-2 w-full max-w-[320px] hover:scale-105 transition-transform transform-gpu duration-500 rounded-md font-medium text-center bg-gray-50 dark:bg-neutral-800/50 border border-blue-300"
         to="food"
       >
         Bag Expansion
