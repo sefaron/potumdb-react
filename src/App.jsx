842026eb43fc65d95d5019019b450742bf305743
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-05 23:39:09 +0600

    update 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./component/header/Header";
 import { Routes, Route } from "react-router";
+import Error404 from "./pages/404/error404";
 import Home from "./pages/home/Home";
 import Food from "./pages/food/Food";
 
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="food" element={<Food />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </>
   );
---
+++ src/pages/404/error404.jsx
@@ -0,0 +1,12 @@
+function Error404() {
+  return (
+    <main className="flex flex-col items-center justify-center p-6">
+      <h1 className="text-5xl text-center my-20 text-wrap">
+        Page not found 😔
+      </h1>
+      <h3 className="text-2xl">Error 404</h3>
+    </main>
+  );
+}
+
+export default Error404;
