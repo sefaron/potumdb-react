ed005ed9278e108d53d6e3986c0235d65b73642c
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-05 23:08:35 +0600

    set 404 page to home

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="food" element={<Food />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </>
   );
