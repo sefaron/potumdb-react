05bd17dc731ba227f7a9406d743fba09c64fc9ea
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-06 18:22:01 +0600

    update error 404

--- src/pages/404/error404.jsx
+++ src/pages/404/error404.jsx
@@ -1,11 +1,24 @@
 function Error404() {
   return (
-    <main className="flex flex-col items-center justify-center p-6">
-      <h1 className="text-5xl text-center my-20 text-wrap">
-        Page not found 😔
-      </h1>
-      <h3 className="text-2xl">Error 404</h3>
-    </main>
+    <>
+      <img
+        className="w-full z-[-1] blur-sm sm:blur-md fixed bg-cover bg-center top-50 sm:top-30"
+        src="/images/404.webp"
+        alt=""
+      />
+      <main className="min-h-full flex flex-col items-center justify-center p-6 overflow-hidden">
+        <h1 className="text-4xl text-white font-bold text-center my-12 text-wrap">
+          If you want it,
+        </h1>
+        <h1 className="text-4xl text-white font-bold text-center my-8 text-wrap">
+          then you'll have to take it.
+        </h1>
+        <h1 className="text-4xl text-white font-bold text-center my-8 text-wrap">
+          But you already knew that.
+        </h1>
+        <h3 className="text-sm text-white fixed bottom-15">Error 404</h3>
+      </main>
+    </>
   );
 }
 
