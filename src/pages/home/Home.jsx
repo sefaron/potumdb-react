14568000a2a2a769742b8c4447625d44d0d2786b
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-07 13:37:39 +0600

    fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
         Quest Calulator
       </Link>
       <Link
-        className="block text-2xl p-[0.8rem] w-full max-w-[320px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
+        className="block text-2xl p-[0.8rem] w-[60dvw] sm:w-[420px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
         to="bag"
       >
         Bag Expansion
