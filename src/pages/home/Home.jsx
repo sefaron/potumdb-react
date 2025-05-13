4b47e9cdbf934bd004dd2812fe7e60115102364d
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-13 14:54:49 +0600

    update margin to work with new padding

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 function Home() {
   return (
-    <main className="flex flex-col gap-6 justify-center items-center my-16">
+    <main className="flex flex-col gap-6 justify-center items-center my-14">
       <Link
         className="block text-2xl p-[0.8rem] w-[70dvw] sm:w-[420px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
         to="food"
