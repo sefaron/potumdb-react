99f5811698e92d94039219ff7c9f92c302db17a7
Lix <33119700+Lixeira@users.noreply.github.com>
2025-05-14 22:12:46 +0600

    add static xp calc page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,12 +9,12 @@ function Home() {
       >
         Food Buff
       </Link>
-      <Link
+      <a
+        href="https://potumdb.pages.dev/xp"
         className="block text-2xl p-[0.8rem] w-[70dvw] sm:w-[420px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
-        to="food"
       >
         Quest Calulator
-      </Link>
+      </a>
       <Link
         className="block text-2xl p-[0.8rem] w-[70dvw] sm:w-[420px] hover:scale-103 transition-transform duration-500 rounded-md font-medium text-center bg-gray-50 hover:bg-gray-200 dark:bg-neutral-800/50 dark:hover:bg-neutral-700/50 border border-blue-300 shadow-2xs"
         to="bag"
