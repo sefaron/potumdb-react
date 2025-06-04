b5ecbf66cff526d8e9bcddd5376a3cb5859bbdc6
ef <33119700+sefaron@users.noreply.github.com>
2025-06-04 14:02:42 +0600

    feat(home)update xp link

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -14,7 +14,7 @@ function Home() {
           Food Buff
         </Link>
         <a
-          href="https://potumdb.pages.dev/xp"
+          href="https://potumdb-xp.pages.dev"
           className="text-xl p-3 w-[70dvw] sm:w-[420px] hover:shadow-lg hover:transition-all hover:duration-500 rounded-md font-medium text-center bg-light-primary hover:bg-light-secondary dark:bg-dark-primary dark:hover:bg-dark-secondary border border-blue-300 shadow-2xs"
         >
           Quest Calulator
