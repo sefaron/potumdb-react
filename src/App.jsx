636dbcb47878615cc98f2d5dae489071b44426c8
ef <33119700+sefaron@users.noreply.github.com>
2025-07-17 14:38:59 +0600

    feat : route unknown links back to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router";
-import Error404 from "./pages/404/error404";
 import Home from "./pages/home/home";
 import Food from "./pages/food/food";
 import Bag from "./pages/bag/bag";
@@ -11,7 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="food" element={<Food />} />
         <Route path="bag" element={<Bag />} />
-        <Route path="*" element={<Error404 />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </>
   );
